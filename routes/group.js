2a50b69bcc35b8507666a2415976a9f240585e70
LordAshan <ashanek99@gmail.com>
2022-06-04 16:27:46 +0530

    group update

--- routes/group.js
+++ routes/group.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { addGroup, getGroup,verifyGroupList } from '../controllers/group.js';
+import { addGroup, getGroup,verifyGroupList,getGroups,acceptTopic,deniedTopic,addCoSupervisor,getGroupsCoSupervisor} from '../controllers/group.js';
 
 
 const router = express.Router();
@@ -7,5 +7,10 @@ const router = express.Router();
 router.post('/addGroup', addGroup);
 router.get('/getGroup/:id',getGroup) 
 router.post('/verify', verifyGroupList);
+router.get('/getGroups',getGroups);
+router.post('/topicA/:id',acceptTopic);
+router.post('/topicD/:id',deniedTopic );
+router.get('/getCoSupervisor',getGroupsCoSupervisor);
 
+ 
 export default router;
